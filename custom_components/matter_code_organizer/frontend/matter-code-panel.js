e14acb25ef4277aefd79b9a856a1c7a84aeacec3
Oliver Busch <info@oliver-busch.com>
2026-03-15 23:04:36 +0100

    Move Export PDF button into Backup/Restore dropdown menu

--- custom_components/matter_code_organizer/frontend/matter-code-panel.js
+++ custom_components/matter_code_organizer/frontend/matter-code-panel.js
@@ -553,10 +553,6 @@ class MatterCodePanel extends HTMLElement {
           <span class="version-badge">v${this._escHtml(this._version)}</span>
         </div>
         <div class="toolbar-actions">
-          <button id="btn-export-pdf" ${this._devices.length === 0 ? "disabled" : ""}>
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 7.5c0 .83-.67 1.5-1.5 1.5H9v2H7.5V7H10c.83 0 1.5.67 1.5 1.5v1zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5v3zm4-3H19v1h1.5V11H19v2h-1.5V7h3v1.5zM9 9.5h1v-1H9v1zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm10 5.5h1v-3h-1v3z"/></svg>
-            <span class="btn-text">${this._t("exportPdf")}</span>
-          </button>
           <div class="toolbar-dropdown">
             <button id="btn-backup-restore">
               <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor"><path d="M12 3C7.58 3 4 6.58 4 11H1l3.89 3.89.07.14L9 11H6c0-3.31 2.69-6 6-6s6 2.69 6 6-2.69 6-6 6c-1.66 0-3.14-.69-4.22-1.78L6.34 16.66C7.9 18.24 9.83 19 12 19c4.42 0 8-3.58 8-8s-3.58-8-8-8zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H11z"/></svg>
@@ -564,6 +560,10 @@ class MatterCodePanel extends HTMLElement {
             </button>
             ${this._showBackupMenu ? `
               <div class="toolbar-dropdown-menu" id="backup-dropdown-menu">
+                <button id="btn-dropdown-export-pdf" ${this._devices.length === 0 ? "disabled" : ""}>
+                  <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M20 2H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-8.5 7.5c0 .83-.67 1.5-1.5 1.5H9v2H7.5V7H10c.83 0 1.5.67 1.5 1.5v1zm5 2c0 .83-.67 1.5-1.5 1.5h-2.5V7H15c.83 0 1.5.67 1.5 1.5v3zm4-3H19v1h1.5V11H19v2h-1.5V7h3v1.5zM9 9.5h1v-1H9v1zM4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm10 5.5h1v-3h-1v3z"/></svg>
+                  ${this._t("exportPdf")}
+                </button>
                 <button id="btn-download-backup">
                   <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg>
                   ${this._t("downloadBackup")}
@@ -841,7 +841,7 @@ class MatterCodePanel extends HTMLElement {
       this._render();
     });
 
-    $("#btn-export-pdf")?.addEventListener("click", () => this._exportPdf());
+    $("#btn-dropdown-export-pdf")?.addEventListener("click", () => { this._showBackupMenu = false; this._exportPdf(); });
 
     // Backup / Restore dropdown
     $("#btn-backup-restore")?.addEventListener("click", (e) => {
